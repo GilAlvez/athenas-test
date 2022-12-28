da85c98d49ab8e1779e18806cbee0f41eaa94587
Gil Alves <gildson_af@outlook.com>
2022-12-28 19:51:53 -0300

    feat(libs): Import libs required CSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import "../styles/globals.css";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'devextreme/dist/css/dx.light.css';
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	return <Component {...pageProps} />;
 }
