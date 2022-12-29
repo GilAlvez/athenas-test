920d42c0143e2e34f9d61bd953dfeb1ea5aa099f
Gil Alves <gildson_af@outlook.com>
2022-12-29 16:04:22 -0300

    feat(page): Integrated user API to DataGrid and pagination

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,34 @@
+import axios from 'axios';
 import DataGrid, { Column, SearchPanel } from 'devextreme-react/data-grid';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CreateModal from '../components/Modals/CreateModal';
 import RemoveModal from '../components/Modals/RemoveModal';
 import ViewModal from '../components/Modals/ViewModal';
-import { pagination } from '../data/pagination';
-import { listAllUsers } from '../data/users';
 
 export default function Home() {
 	const [selectedUser, setSelectedUser] = useState<number>();
+	const [data, setData] = useState({ users: [], pagination: { total: 0, current: 0 } });
+	const [loading, setLoading] = useState(false);
+	const [page, setPage] = useState(1);
+
+	// Fetch Data
+	useEffect(() => {
+		const getAllUsers = async () => {
+			try {
+				setLoading(true);
+				const { data } = await axios.get('/api/v1/users', { params: { page } });
+				setData(data);
+			} catch (error: any) {
+				alert(error.response.data.message);
+			} finally {
+				setLoading(false);
+			}
+		};
+		getAllUsers();
+	}, [page]);
+
+	// Transform totalPages into a array of numbered pages, and ignore index 0
+	const arrayOfPages = Array.from({ length: data.pagination.total }).map((_, index) => index + 1);
 
 	// Modal Handlers
 	const createUserModal = () => {
@@ -48,55 +69,67 @@ export default function Home() {
 		);
 	};
 
-	// Pagination Treatment
-	// Transform totalPages into a array of numbered pages, and ignore index 0
-	const arrayOfPages = Array.from({ length: pagination.totalPages }).map((_, index) => index + 1);
-
 	return (
 		<section>
 			<button type="button" className="btn btn-primary" onClick={() => createUserModal()}>
 				Create new user
 			</button>
 
-			<DataGrid
-				id="dataGrid"
-				dataSource={listAllUsers}
-				allowColumnReordering
-				columnAutoWidth
-				rowAlternationEnabled
-				showBorders
-			>
-				<SearchPanel visible highlightCaseSensitive />
-
-				<Column dataField="name" />
-				<Column dataField="age" />
-				<Column dataField="gender" />
-				<Column dataField="address" />
-				<Column dataField="birthday" />
-				<Column
-					dataField="id"
-					caption="Actions"
-					cssClass={'d-flex justify-content-center gap-2'}
-					cellRender={RenderActionsCell}
-				/>
-			</DataGrid>
-
 			{/* PAGINATION */}
 			<nav aria-label="Page navigation example">
 				<ul className="mt-2 pagination justify-content-end">
 					<li className="page-item">
-						<span className="page-link">Prev</span>
+						<span className="page-link" onClick={() => page > 1 && setPage((page) => page - 1)}>
+							Prev
+						</span>
 					</li>
 					{arrayOfPages.map((page) => (
 						<li className="page-item" key={page}>
-							<span className={`page-link ${pagination.current === page && 'active'}`}>{page}</span>
+							<span
+								className={`page-link ${data.pagination.current === page && 'active'}`}
+								onClick={() => setPage(page)}
+							>
+								{page}
+							</span>
 						</li>
 					))}
 					<li className="page-item">
-						<span className="page-link">Next</span>
+						<span
+							className="page-link"
+							onClick={() => page < data.pagination.total && setPage((page) => page + 1)}
+						>
+							Next
+						</span>
 					</li>
 				</ul>
 			</nav>
+
+			{loading ? (
+				<h3 className="my-3">Carregando</h3>
+			) : (
+				<DataGrid
+					id="dataGrid"
+					dataSource={data.users}
+					allowColumnReordering
+					columnAutoWidth
+					rowAlternationEnabled
+					showBorders
+				>
+					<SearchPanel visible highlightCaseSensitive />
+
+					<Column dataField="name" />
+					<Column dataField="age" />
+					<Column dataField="gender" />
+					<Column dataField="address" />
+					<Column dataField="birthday" dataType="date" />
+					<Column
+						dataField="id"
+						caption="Actions"
+						cssClass={'d-flex justify-content-center gap-2'}
+						cellRender={RenderActionsCell}
+					/>
+				</DataGrid>
+			)}
 
 			{/* MODALS */}
 			<ViewModal id={selectedUser} />
