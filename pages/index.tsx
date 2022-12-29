65f369a7d8cc686fe9f29e14884b43816bc1f013
Gil Alves <gildson_af@outlook.com>
2022-12-28 21:35:28 -0300

    feat(component): Add DevExpress DataGrid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,52 @@
+import DataGrid, { Column, SearchPanel } from 'devextreme-react/data-grid';
+import { listAllUsers } from '../data/users';
+
 export default function Home() {
-	return <></>;
+	const RenderActionsCell = () => {
+		return (
+			<>
+				<button
+					type="button"
+					className="btn btn-sm btn-primary bi bi-pencil-fill"
+					aria-label="View"
+				/>
+				<button
+					type="button"
+					className="btn btn-sm btn-danger bi bi-trash-fill"
+					aria-label="Remove"
+				/>
+			</>
+		);
+	};
+
+	return (
+		<section>
+			<button type="button" className="btn btn-primary">
+				Create new user
+			</button>
+
+			<DataGrid
+				id="dataGrid"
+				dataSource={listAllUsers}
+				allowColumnReordering
+				columnAutoWidth
+				rowAlternationEnabled
+				showBorders
+			>
+				<SearchPanel visible highlightCaseSensitive />
+
+				<Column dataField="name" />
+				<Column dataField="age" />
+				<Column dataField="gender" />
+				<Column dataField="address.street" />
+				<Column dataField="birthday" />
+				<Column
+					dataField="id"
+					caption="Actions"
+					cssClass={'d-flex justify-content-center gap-2'}
+					cellRender={RenderActionsCell}
+				/>
+			</DataGrid>
+		</section>
+	);
 }
