3bcbcd81e96c27aabbbe7919b29777bbc84a9305
Gil Alves <gildson_af@outlook.com>
2022-12-29 09:16:07 -0300

    feat(components): Add form controls to modals

--- components/Modals/ViewModal.tsx
+++ components/Modals/ViewModal.tsx
@@ -1,4 +1,18 @@
+import { useState } from 'react';
+
 const EditModal = ({ id }: { id?: number }) => {
+	const [values, setValues] = useState<{
+		name?: string;
+		age?: string;
+		gender?: string;
+		address?: string;
+		birthday?: string;
+	}>();
+
+	const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+		const { name, value } = e.currentTarget;
+		setValues({ ...values, [name]: value });
+	};
 	return (
 		<div
 			className="modal fade"
@@ -20,7 +34,66 @@ const EditModal = ({ id }: { id?: number }) => {
 							aria-label="Close"
 						></button>
 					</div>
-					<div className="modal-body">...</div>
+					<div className="modal-body">
+						<div className="row g-2">
+							<div className="col-10">
+								<label htmlFor="name">Name</label>
+								<input
+									id="name"
+									name="name"
+									type="text"
+									className="form-control"
+									value={values?.name}
+									onChange={handleInputChange}
+								/>
+							</div>
+							<div className="col-2">
+								<label htmlFor="age">Age</label>
+								<input
+									id="age"
+									name="age"
+									type="number"
+									className="form-control"
+									value={values?.age}
+									onChange={handleInputChange}
+								/>
+							</div>
+							<div className="col-6">
+								<label htmlFor="gender">Gender</label>
+								<input
+									id="gender"
+									name="gender"
+									type="text"
+									className="form-control"
+									value={values?.gender}
+									onChange={handleInputChange}
+								/>
+							</div>
+
+							<div className="col-6">
+								<label htmlFor="birthday">Birthday</label>
+								<input
+									id="birthday"
+									name="birthday"
+									type="date"
+									className="form-control"
+									value={values?.birthday}
+									onChange={handleInputChange}
+								/>
+							</div>
+							<div className="col-12">
+								<label htmlFor="address">Address</label>
+								<input
+									id="address"
+									name="address"
+									type="text"
+									className="form-control"
+									value={values?.address}
+									onChange={handleInputChange}
+								/>
+							</div>
+						</div>
+					</div>
 					<div className="modal-footer">
 						<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
 							Close
