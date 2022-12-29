3fc6f27da3fbd86b529a8c2db53dae0fc632d33e
Gil Alves <gildson_af@outlook.com>
2022-12-29 09:25:07 -0300

    feat(http): Add basic CORS config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
+	headers: async () => {
+		return [
+			{ source: '/api/v1/:path*', headers: [{ key: 'Access-Control-Allow-Origin', value: '*' }] },
+		];
+	},
 };
 
 module.exports = nextConfig;
